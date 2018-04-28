c350d5999fa74ab903209d59c4f6ee31a7e44f76
aayush6194 <36089580+aayush6194@users.noreply.github.com>
2018-04-28 13:51:38 -0500

    Add files via upload

--- WordPairs.java
+++ WordPairs.java
@@ -5,9 +5,7 @@
   * Instructor: Dr. Cordova              
   * File name: WordPairs         
   *
-  * Program Description: This program  that a given a list of animals to be transported, finds the smallest
-  *                      number of containers needed to complete the shipment. 
-                          
+                      
   * Plagiarism Statement:
   * I swear that I have not received or given aid on this program
   * beyond guidelines of the course including giving or receiving
@@ -15,11 +13,30 @@
   */
   
 /**
- * Importing libraries 
+ * This program  that a given a list of words and it's edges, finds the 
  */
 import java.util.*;
 import java.io.*;
 
+
+
+/**
+ * Program header and description:
+ *
+ * A wordPairs class that a given a list of words and following words (vertex and edges connection) reads the file and uses DiGraph class for a
+ * adjecency list representation of a graph. Also additonal Word class objects are used to store the node level and node's word chain(word pairs) for each word.
+ *   
+ * WordPair class uses breadth for search algorithm to reach a required node or a required level of nodes as per the method that calls the bfs method.
+ * Also, to find of the nodes in a certain level a Level map is used that takes int (level of the word) and word class object to store nodes (alphabetically) in a set 
+ * per the level of the node. Map key is traversed by a for loop to retrive elements in each level.
+ * To find of the stortest word chain or chain length till a certain node is traversed, a wordMap map is used that takes string (name of the word) and word class object to store chain  in as a string.
+ * Chain is stored as string and not array to aviod passing an array per every word(node) as argument(because of java's type checking).
+ * Additonally, a visted set is used to keep track of the visted node. 
+ * The chain string is retrived by updating a globalChain varible by bfs method, it is reset before each use to aviod any stale data.
+ * To use a common bfs method for each method, it uses diffrent arguments to target a required data.
+ * String last is pass as null if the method does not require finding an element
+ * int maxLevel is pass as -1 if the method does not require reaching a certain level in the graph
+ */
 public class WordPairs
 {
    /**
@@ -29,7 +46,7 @@ public class WordPairs
    private String globalChain;
 
    /**
-    * A constructor that initializes containers and animallist, and calls a createList method
+    * A constructor that initializes variable and DiGraph object and calls a createList method
     * @param filename, a string that is the name of file to be opened
     */
    public WordPairs (String name){
@@ -110,7 +127,7 @@ public class WordPairs
   * that begins with first and ends with last.
   * @param first, a string that first starting node (word) or the root for the traversal 
   * @param last, a string that last node (word) or the last child for the traversal 
-  * @return the number of words pairs in the shortest chain that begins with first and ends with last.
+  * @return an int, that is the number of words pairs in the shortest chain that begins with first and ends with last.
   */
    public int chainLength(String first, String last){
       return  bfs(first, last, -1);
@@ -120,7 +137,7 @@ public class WordPairs
   * A reachableForm method that takes word and returns number of distinct words in the chain from the traversal that begins with word. 
   * that begins with first and ends with last.
   * @param word, a string that first starting node (word) or the root for the traversal 
-  * @return the number of  distinct words  in the chain from the traversal that begins with word.
+  * @return an int, that is the number of  distinct words  in the chain from the traversal that begins with word.
   */   
    public int reachableFrom(String word){
       return reachableFrom(word, -1);
@@ -132,7 +149,7 @@ public class WordPairs
   * that begins with first and ends with last.
   * @param word, a string that first starting node (word) or the root for the traversal
   * @param maxLength, an int that is max number of distinct words in the chain
-  * @return the number of  distinct words  in the chain from the traversal that begins with word.
+  * @return an int, that is the number of  distinct words  in the chain from the traversal that begins with word.
   */   
    public int reachableFrom(String word, int maxLength){
       return bfs(word, null, maxLength);
@@ -143,7 +160,7 @@ public class WordPairs
   * begins with word and returns distinct words in the chain from the traversal that begins with word that begins with first and ends with last.
   * @param word, a string that first starting node (word) or the root for the traversal
   * @param maxLength, an int that is max number of distinct words in the chain
-  * @return a string distinct words  in the chain from the traversal that begins with word.
+  * @return a string, that is the distinct words  in the chain from the traversal that begins with word.
   */ 
    public String reachableWords(String word, int maxLength){
      /**
@@ -158,7 +175,7 @@ public class WordPairs
   * A cycle method that takes  word and returns the shortest sequence of word chain 
   * that begins with word and also ends with word.
   * @param word, a string that first starting node (word) or the root for the traversal, also the last node (word) or the last child for the traversal  
-  * @return a string that is  shortest chain of word pairs that begins with word and ends with word,using a certain format.
+  * @return a string, that is  shortest chain of word pairs that begins with word and ends with word,using a certain format.
   */ 
    public String cycle(String word){
      /**
@@ -174,9 +191,11 @@ public class WordPairs
   * that begins with first and ends with last.
   * @param first, a string that first starting node (word) or the root for the traversal 
   * @param last, a string that last node (word) or the last child for the traversal 
-  * @return the number of distint words or wordpairs in the shortest chain that begins with first and ends with last
+  * @param maxLevel, number of level till which the nodes are traversed
+  * @return an int, that is the number of distint words or wordpairs in the shortest chain that begins with first and ends with last (if last exist)
+  * return int values depends on the value of last and maxLevel
   */       
-   public int bfs(String vertex, String last, int maxLevel){
+   private int bfs(String vertex, String last, int maxLevel){
    /**
     * Declaring and initilizing variables and data structures objects
     */       
@@ -184,10 +203,12 @@ public class WordPairs
       int reachedLevel = 1;
       boolean reached = false, found = false, first = true;  
       LinkedList <String> q = new LinkedList <String>();
-      
-      //wordChain
+   /**
+    * wordMap stores each word as string and it's length and chain. Level stores words in each level
+    */   
       Map <String, Words> wordMap = new HashMap <String, Words>();
       Map <Integer, Words> level = new HashMap <Integer, Words>(); 
+      //visted
       Set <String> visted = new HashSet<String>();
    /**
     * Addting the first node to the data structures objects
@@ -201,44 +222,71 @@ public class WordPairs
    /**
     * while loop iterates till the queue is empty or the last node is found or the desire level is reached
     */   
-      while(  !q.isEmpty() && !reached)
-      {
+      while(!q.isEmpty() && !reached){      
+        /**
+         * Declaring and initilizing variables
+         */   
          parent = q.poll();
          String currentChain = wordMap.get(parent).getChain();
          int currentLevel = wordMap.get(parent).getLevel();
       
+         /**
+         * checking for the end condition and setting the flags
+         */           
          if( (currentLevel -1 == maxLevel || (parent.equals(last)) && (!first))){ 
             reachedLevel = currentLevel -1 ;
             reached = true;
             found = parent.equals(last);
          }
-         else                   
-         {
-            for(String each: graph.getAdjacent(parent)){
-            
+         else  {
+         
+           /**
+            * iterates the nearest child node
+            */           
+            for(String each: graph.getAdjacent(parent)){            
+               /**
+                * checking if the node has been visted
+                */  
                if(!visted.contains(each) ){
+               
+               /**
+                * adding the node to the queue, visted set, level and wordMap
+                */   
                   q.add(each);          
                   wordMap.put(each, new Words(currentLevel +1, currentChain, parent +" " +each));
-                  visted.add(each);               
+                  visted.add(each);    
+                /**
+                 * checking if level map has the key
+                 */                          
                   if(!level.containsKey(currentLevel+1))
                      level.put(currentLevel+1, new Words(each));   
                   else 
                      level.get(currentLevel+1).addWord(each);               
                }
-             
             }
             first = false;
          }         
       }
-          
+      
+     /**
+      * last string is null when the end word is indefined and number of distinct word is returned
+      */      
       if (last == (null))      
          return wordMap.size();
       
-      else if(last.equals("-1"))
-      {
-         globalChain = "";
-         for(int i = 1; i <= maxLevel+1; i++)
-         {
+     /**
+      * last string is "-1" when the end word is indefined and globalChain is updated with distint words as each level and 0 is returned
+      * It is done to  aviod unnecessarily updating the globalChain if the method does not require it
+      */  
+      else if(last.equals("-1")){
+         globalChain = "";      
+      /**
+       * iterates the each level of the map
+       */   
+         for(int i = 1; i <= maxLevel+1; i++){
+          /**
+           * if a level in a map isnt empty
+           */
             if(level.containsKey(i))
                globalChain +=(Arrays.toString(level.get(i).getWordLevel())) +"\n";
             else
@@ -246,8 +294,13 @@ public class WordPairs
          }
          return 0;
       }
-      else if(found)
-      {
+     /**
+      * last string is not -1 or null the globalChain is updated with distint words pairs and number of word pairs is returned
+      */
+      else if(found){
+      /**
+       * globalChain is updated with the chain of the last element and return the reachedLevel
+       */ 
          globalChain = wordMap.get(last).getChain(); 
          if(globalChain.length() > 0)
             globalChain = "[" + globalChain.substring(0, globalChain.length()-1) + "]";
@@ -255,6 +308,9 @@ public class WordPairs
          return reachedLevel;
       }
       
+      /**
+       * globalChain is updated with the "[]" if there is no link and returns max integer
+       */ 
       globalChain = "[]";
       return Integer.MAX_VALUE;
    }
--- Words.java
+++ Words.java
@@ -1,43 +1,80 @@
+/**
+ * Importing libraries 
+ */
 import java.util.*;
-public class Words
-{
+public class Words{
 
+   /**
+    * Declaring instance variable and set object. Set object stores distinct words in a level
+    */ 
    private int level;
    private String chain;
    Set <String> worldLevel;
+   boolean visted;
    
-   public Words (){
+   /**
+    * A no argument constructor that initializes instance variable and set object
+    */ 
+   public Words (boolean visted){
     level = 1; 
      worldLevel = new TreeSet <String>(); 
     chain = "";
+    this.visted = visted;
+    
    }
 
+   /**
+    * An argument constructor that initializes instance variable and set object
+    * @param level, the level at which the word is found in the graph
+    * @param chain, a string that is the chain of the parent element
+    * @param parent, a string that is the name of the parent element
+    */ 
    public Words (int level, String chain, String parent){
    this.level = level; 
    worldLevel = new TreeSet <String>();
    this.chain = chain + parent +"," ;
+   visted = true;
    }
-   
+ 
+    /**
+    * An argument constructor that initializes the set object and adds the word to the set
+    * @param word, a string that is the name of the distinct word in a level
+    */   
    public Words(String word){
      worldLevel = new TreeSet <String>();
      worldLevel.add(word);
    }
 
+   
+ /**
+  * A getLevel method that returns the level at which the certain word is found in the Digraph object 
+  * @return an int the level at which the word is found in the graph  
+  */  
    public int getLevel(){
    return level;  
    }
-   
+ 
+ /**
+  * A getLevel method that returns a string that is the chain of the element the word is associated to
+  * @return a string that is the chain of the element
+  */    
    public String getChain(){
    return chain;
    }
    
-   
+  /**
+  * A getWordLevel method that returns a string array that is the elements of a particular level
+  * @return a string array that is the elements of a particular level
+  */   
    public String [] getWordLevel(){
    return worldLevel.toArray(new String[worldLevel.size()]);
    }
-   
+ 
+   /**
+    * An addWord method that add a word to the set object
+    * @param word, a string that is the name of the distinct word in a level
+    */     
    public void addWord (String word){
-  
      worldLevel.add(word);
    }
   
